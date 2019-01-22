8fab3518b4592a9c19188f9e0859fa47a9f1929d
Reece Dunham <me@rdil.rocks>
2019-01-22 10:29:13 -0500

    fix that

--- masmas.js
+++ masmas.js
@@ -1,16 +1,16 @@
 /**
-  Credits:
-  ~ N8Python - isFloat, isInteger, Int, Float, execute, globalVar, exists,
-    Number.prototype.A, Number.prototype.times, localStore, 
-    wrap, MasMasCanvas, type
-  ~ user113716 - String.prototype.splice
-  ~ Ghostoy - commmafy
-  ~ Lavamantis - Number.prototype.round
-  ~ RDIL - Fix loadjQuery
-  ~ Sam Deering - loadScript
-  ~ kieranpotts - isPrimitive
-  [{ Insert your username and functions you contribute up here. }]
-**/
+  * Credits:
+  * ~ N8Python - isFloat, isInteger, Int, Float, execute, globalVar, exists,
+  * Number.prototype.A, Number.prototype.times, localStore, 
+  * wrap, MasMasCanvas, type
+  * ~ user113716 - String.prototype.splice
+  * ~ Ghostoy - commmafy
+  * ~ Lavamantis - Number.prototype.round
+  * ~ RDIL - Fix loadjQuery
+  * ~ Sam Deering - loadScript
+  * ~ kieranpotts - isPrimitive
+  * [{ Insert your username and functions you contribute up here. }]
+  */
 
 function isFloat (n) {
   return n === +n && n !== (n | 0);
