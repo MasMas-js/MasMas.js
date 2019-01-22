64d8945993a8bedcdf5b1c217f552b8f7164b1b0
n8programs <43304488+N8python@users.noreply.github.com>
2019-01-22 15:50:52 -0500

    Update masmas.js

--- masmas.js
+++ masmas.js
@@ -139,11 +139,12 @@ class Random {
     }
     return a;
   }
-  getRandomFloat(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-  getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  getRandomNumber(min, max, float=false) {
+    if(float){
+      return Math.random() * (max - min) + min;
+    } else {
+      return Math.floor(Math.random() * (max - min + 1) + min);          
+    }
   }
   getRandomBool() {
     return Math.random() >= 0.5;
