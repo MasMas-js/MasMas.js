1218f9c000b8bbcfc6695b2b8fc9658ad1ca2eb1
Reece Dunham <me@rdil.rocks>
2019-01-22 08:59:58 -0500

    ok?

--- masmas.js
+++ masmas.js
@@ -12,11 +12,11 @@
 **/
 
 function isFloat(n) {
-  return n === +n && n !== (n | 0);
+  return n === +n && n !== (n | 0)
 }
 
 function isInteger(n) {
-  return n === +n && n === (n | 0);
+  return n === +n && n === (n | 0)
 }
 
 function Int(n) {
